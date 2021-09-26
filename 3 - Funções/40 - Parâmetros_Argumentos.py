cd1a4fe2a3d35fa31ce9f256a19bfee95f49a71b
Danielly Viana <89473256+DanyViana@users.noreply.github.com>
2021-09-26 18:44:40 -0300

    mais parâmetros

--- 3 - Funções/40 - Parâmetros_Argumentos.py
+++ 3 - Funções/40 - Parâmetros_Argumentos.py
@@ -8,3 +8,29 @@ def boas_vindas(nome, quantidade): #dentro do () vão ter os parâmetros
 boas_vindas('Dany',5) #quando chamar a função você defini quais os parâmetros que ela deve chamar. 
 boas_vindas('Gafanhoto',10)
 boas_vindas('Pessoa',2)
+
+def quantidades_deful(nome, quantidade = 6): #dentro do () vão ter os parâmetros, o parâmetro de quantidade é default, não será alterado. E o de nome será no-default. 
+    #Sempre coloque na seguinte ordem: parâmetros no-default e depois os default
+    print(f'Olá, {nome}.')
+    print(f'Temos {str(quantidade)} PCs disponíveis.')
+
+quantidades_deful('Dani') #se eu não definir ele vai vim como 6
+quantidades_deful('teste', 4) #se eu definir o parâmetro de quantidade ele subescreve
+
+def mais_de_uma(nome, quantidade = 3, local = 'almrep'): #dentro do () vão ter os parâmetros
+    print(f'Olá, {nome}.')
+    print(f'Temos {str(quantidade)} PCs disponíveis no {local}.')
+
+mais_de_uma('Dani', 2 , 'almox')
+
+def cliente1(nome):
+    print(f'Olá {nome}')
+
+def cliente2(nome):
+    return f'Olá {nome}'
+
+x = cliente1('Maria')
+y = cliente2('José')
+
+print(x) #Retorna como 'none' pois a função print só realiza uma tarefa - retorna um valor na tela, mas no programa em si não retorna nada
+print(y) #A função return calcula e retorna um valor (fica armazenado)
