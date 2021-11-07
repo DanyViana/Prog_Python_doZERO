0ead19c5a6782c5b331b317e3a67ecbf18ab06cb
Danielly Viana <89473256+DanyViana@users.noreply.github.com>
2021-11-07 18:03:52 -0300

    Create 45 - Importando_módulo.py
    módulo math

---
+++ 3 - Funções/45 - Importando_módulo.py
@@ -0,0 +1,14 @@
+# Importando um módulo matemático
+
+# Qual é o número fatorial de 4? 
+
+fatorial = 4*3*2*1
+print(fatorial)
+
+# E se fosse pra fazer do número 57? Cê ta doido de fazer na mão?! Importe a biblioteca math. 
+
+import math
+
+print(math.factorial(11))
+
+# Pesquise no google o que você quer! 
