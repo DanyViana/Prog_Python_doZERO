63c8f759eaed7c075b640b8e6341e610fc672b46
Danielly Viana <89473256+DanyViana@users.noreply.github.com>
2021-11-08 20:45:44 -0300

    Update 46 - Listas.py
    listas

--- 4 - Estrutura de Dados/46 - Listas.py
+++ 4 - Estrutura de Dados/46 - Listas.py
@@ -46,3 +46,46 @@ print(cidades)
 cidades.sort() #organiza em ordem alfabética
 
 print(cidades)
+
+numeros = [2,3,4,5]
+final = numeros * 2 # Vai duplicar a lista
+print(final)
+
+letras = ['a', 'b', 'c', 'd']
+final2= numeros + letras # Concatenar listas
+print(final2)
+
+numeros.extend(letras) # Estendeu a lista de números com a lista de letras
+print(numeros)
+
+itens = ['item1', 'item2', 'item3', 'item4']
+print(itens)
+
+itens2 = [['item1', 'item2'], ['item3', 'item4']]
+# Uma lista dentro de outra lista, a lista maior tem dois index (0 e 1), e as sublistas também tem dois index (0 e 1)
+
+print(itens2[0][1]) # Retorna ao valor do index 0 da lista maior na posição 1
+
+produtos = ['arroz', 'feijão', 'laranja', 'banana']
+item1 = produtos[0]
+print(item1)
+
+item1, item2, item3, item4 = produtos # Todas as variáveis associadas a lista
+print(item1)
+print(item2)
+print(item3)
+print(item4)
+
+produtos2 = ['arroz', 'feijão', 'laranja', 'banana', 2, 3, 4]
+item1, item2, item3, *outros = produtos2 # Para referenciar apenas alguns dos itens da lista
+print(item1)
+print(item2)
+print(item3)
+print(outros)
+
+produtos3 = ['arroz', 'feijão', 'laranja', 'banana', 2, 3, 4]
+item1, item2, *outros, item6 = produtos3 # Para referenciar apenas alguns dos itens da lista
+print(item1)
+print(item2)
+print(outros)
+print(item6)
