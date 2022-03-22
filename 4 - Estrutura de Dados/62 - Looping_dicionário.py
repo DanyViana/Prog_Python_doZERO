a0db52797f46c8e99ef38c56f568753b5a0671a4
Danielly Viana <89473256+DanyViana@users.noreply.github.com>
2022-03-22 14:36:14 -0300

    Create 62 - Looping_dicionário.py
    looping nos dicionários

---
+++ 4 - Estrutura de Dados/62 - Looping_dicionário.py
@@ -0,0 +1,34 @@
+aluno = {'nome': 'Ana', 'idade': 16, 'nota_final': 'A', 'aprovação': True}
+
+for x in aluno:
+    print(x) #Impressão das keys
+
+#Outro jeito de escrever seria: for x in aluno.keys()
+
+for x in aluno.values(): 
+    print(x) #Impressão dos valores
+
+for x in aluno.items():
+    print(x) #Impressão dos itens do dicionário
+
+for keys, values in aluno.items():
+    print(keys,values)
+
+aluno = {'nome': 'Ana', 'idade': 16, 'nota_final': 'A', 'aprovação': True, 'materias': ['física','matemática','biologia']}
+print(aluno)
+
+#Jeito mais bonitinho de organizar:
+aluno = {
+    'nome': 'Ana',
+    'idade': 16,
+    'nota_final': 'A',
+    'aprovação': True,
+    'materias': ['física','matemática','biologia']
+    }
+print(aluno.get('materias'))
+
+print(len(aluno)) #Retorna ao número de keys que tem no dicionário
+
+print(aluno.items())
+print(aluno.keys())
+print(aluno.values())
