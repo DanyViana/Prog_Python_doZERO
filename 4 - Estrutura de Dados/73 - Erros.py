7430d12a8c2d6ed98f76d63c9f9005c587cc0106
Danielly Viana <89473256+DanyViana@users.noreply.github.com>
2022-03-28 08:52:00 -0300

    Update 73 - Erros.py
    try, except, else e finally

--- 4 - Estrutura de Dados/73 - Erros.py
+++ 4 - Estrutura de Dados/73 - Erros.py
@@ -1,4 +1,4 @@
-# Erros: Excelente para testes. Não realiza o 'stop' no programa. Mensagem customizada quando encontrar um erro. 
+# Erros: Excelente para testes. Não realiza o 'stop' no programa. Mensagem customizada quando encontrar um erro. Excelentes para o caso do usuário digitar um input incorreto. 
 
 try:
     teste = ['a', 'b', 'c']
@@ -8,3 +8,17 @@ except IndexError:
     print('Index não existe')
 
 # Para isso, é necessário incluir o tray e o except. 
+
+try:
+    valor = int(input('Digite o valor do produto: '))
+    print(valor)
+except ValueError: 
+    print('Por gentileza, informe valores numéricos para o produto.')
+else: 
+    print('Valor informado corretamente.')
+    resultado = valor * 2 
+    print(resultado)
+
+print('mais código abaixo')
+
+#No lugar do else também podemos utilizar o 'finally', ele será executado independente se deu erro ou não no código. 
